229a87a31ba970762f95189138a5f625006a26c7
Victor Henri S. Vargas <103096096+imyudz@users.noreply.github.com>
2024-03-10 17:06:30 -0300

    fix: coingecko connector headers fix

--- app/services/connectors/coingecko_api_connector.py
+++ app/services/connectors/coingecko_api_connector.py
@@ -1,9 +1,9 @@
-from config.async_api import AsyncAPI as __AsyncAPI
+from config.async_api import AsyncAPI as _AsyncAPI
 class CoingeckoAPIConnector: 
     def __init__(self, coingecko_url: str, coingecko_api_key: str) -> None:
-        self.async_api = __AsyncAPI(coingecko_url)
+        self._async_api = _AsyncAPI(coingecko_url)
         self.__apikey = coingecko_api_key
         
-    async def get_coins_list(self, include_platform: bool = False):
-        headers = {"Authorization": f"{self.__apikey}"}
-        return await self.__api.request("GET", f"/coins/list?include_platform={include_platform}", headers=headers)
+    async def get_coins_list(self, include_platform: bool = False) -> :
+        headers = {"x-cg-demo-api-key": f"{self.__apikey}"}
+        return await self._async_api.request("GET", f"/api/v3/coins/list?include_platform={include_platform}", headers=headers)
